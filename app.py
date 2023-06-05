cb51d19b02200df8206d6a3398a720719e754ac6
Jairocustodio-jc <67087787+Jairocustodio-jc@users.noreply.github.com>
2023-06-04 21:31:30 -0500

    A

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/users-profile')
 def users_profile():
-    return render_template('users-profile.html')
-if __name__=='__main__':
-    app.run(debug=True,port=3001)   
+    return render_template('usuario/users-profile.html')
+
+if __name__ == '__main__':
+    app.run()
+
