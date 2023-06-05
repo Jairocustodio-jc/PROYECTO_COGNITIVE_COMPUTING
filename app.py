fd584e0a59a666d4f72f340a239693b00b275b87
Jairocustodio-jc <67087787+Jairocustodio-jc@users.noreply.github.com>
2023-06-04 22:52:03 -0500

    login_register_error

--- app.py
+++ app.py
@@ -9,10 +9,22 @@ def index():
 @app.route('/users-profile')
 def users_profile():
     return render_template('usuario/users-profile.html')
+
 @app.route('/pages-faq')
 def pages_faq():
     return render_template('ayuda/pages-faq.html')
 
+@app.route('/pages-login')
+def pages_login():
+    return render_template('acceso_pagina/pages-login.html')
+
+@app.route('/pages-register')
+def pages_register():
+    return render_template('acceso_pagina/pages-register.html')
+
+@app.route('/pages-error-404')
+def pages_error_404():
+    return render_template('pages-error-404.html')
 
 if __name__ == '__main__':
     app.run()
