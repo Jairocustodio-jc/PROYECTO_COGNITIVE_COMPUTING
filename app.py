85523f1a6e8e84e181eb00927357ccf861d8fc71
Jairocustodio-jc <67087787+Jairocustodio-jc@users.noreply.github.com>
2023-06-04 22:21:23 -0500

    Hasta_el_perfil

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def index():
 @app.route('/users-profile')
 def users_profile():
     return render_template('usuario/users-profile.html')
+@app.route('/pages-faq')
+def pages_faq():
+    return render_template('ayuda/pages-faq.html')
+
 
 if __name__ == '__main__':
     app.run()
